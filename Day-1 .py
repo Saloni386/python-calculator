7e7063162530d0d8306d4b5dc97d4c85e4a54874
Saloni386 <blazingsaloni@gmail.com>
2025-04-10 13:26:33 +0530

    Add files via upload

---
+++ Day-1 .py
@@ -0,0 +1,10 @@
+
+# Ek simple calculator banao jo user se 2 number input le aur unka sum, subtraction, multiplication, division print kare.
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+print("The addition of num1 and num2 is",(num1+num2))
+print("The substraction of num1 and num2 is",(num1-num2))
+print("The multiplication of num1 and num2 is",(num1*num2))
+print("The divison of num1 and num2 is",(num1/num2))
+print("The astrics of num1 and num2 is",(num1**num2))
